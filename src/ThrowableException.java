4b8106ca188458f454da917e8837350f73bf464e
Ashka <aas74@duke.edu>
2017-02-26 14:26:05 -0500

    throwable exception backend

---
+++ src/ThrowableException.java
@@ -0,0 +1,20 @@
+/**
+ * Exceptions.
+ * @author Ashka Stephen
+ *
+ */
+public class ThrowableException extends Exception{
+
+	/**
+	 * Default ID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructor
+	 * @param string s (message to be displayed)
+	 */
+	public ThrowableException(String s){
+		super(s);
+	}
+}
