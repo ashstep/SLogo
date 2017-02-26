a3e0317fd561f2295b61041b76d44933fe364362
Ashka <aas74@duke.edu>
2017-02-26 13:11:09 -0500

    node class comment for usage

--- src/Parse/Node.java
+++ src/Parse/Node.java
@@ -1,46 +1,86 @@
 package Parse;
-
+/**
+ * Node class will be used to parse commands.
+ * @author Ashka Stephen
+ *
+ */
 public class Node {
 	String myVal;
 	Node child1;
 	Node child2;
 	
-	//command to insert a child node given a command string? needed or no
-
+	//command to INSERT a child node given a command string? needed or not?
 	
-	public Node(String value, Node childone, Node childtwo) {
+	
+	/**
+	 * Default constructor
+	 * @param value held in node
+	 * @param left child
+	 * @param right child
+	 */
+	public Node(String value, Node leftChild, Node rightChild) {
 		myVal = value;
-		child1 = childone;
-		child2 = childtwo;
+		child1 = leftChild;
+		child2 = rightChild;
 	}
 
-	
+	/**
+	 * Value getter
+	 */
 	public String getValue() {
 		return myVal;
 	}
+	
+	/**
+	 * Left Child Getter
+	 */
 	public Node getChild1(){
 		return child1;
 	}
+	
+	/**
+	 * Right Child Getter
+	 */
 	public Node getChild2(){
 		return child2;
 	}
+	
+	/**
+	 * Is not a leaf node?
+	 */
 	public Boolean hasChildren(){
 		return ((child1!=null) || (child2 != null));
 	}
+	
+	/**
+	 * Left Child Exists
+	 */
 	public Boolean hasChildOne(){
 		return (child1!=null);
 	}
+	
+	/**
+	 * Right Child Exists
+	 */
 	public Boolean hasChildTwo(){
 		return (child2!=null);
 	}
-	//when using this need to check if it only has one child -> else an error will occur
+	
+	/**
+	 * Add child to first available space -> if both children exists, nothing happens -> edit to check???
+	 */
 	public void addChild(Node childNode) {
-		if (child1 == null) {
+		if (!hasChildOne()) {
 			child1 = childNode;
-		} else {
+		} 
+		if ((!hasChildTwo())) {
 			child2 = childNode;
-		}
+		} 
 	}
+
+	/**
+	 * @return number of children node has
+	 */
 	public Integer numChildren(){
 		int count = 0;
 		if (child1!=null){
@@ -51,6 +91,10 @@ public class Node {
 		}
 		return count;
 	}
+	
+	/**
+	 * Leaf Node?
+	 */
 	public Boolean isLeafNode(){
 		return ((child1==null) && (child2 == null));
 	}
