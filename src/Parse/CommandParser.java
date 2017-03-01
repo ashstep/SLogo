70ec6393f173c5eec195893866a7ebe690e8885c
Ashka <aas74@duke.edu>
2017-03-01 01:07:21 -0500

    recursive tree build

--- src/Parse/CommandParser.java
+++ src/Parse/CommandParser.java
@@ -6,14 +6,13 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
-import javafx.beans.property.ObjectProperty;
 /**
  * Parses inputted commands -> calls relevant commands
  * @author Ashka Stephen
  * 
  * 
  * 
- * TODO:
+ * TODO for me:
  * deal with regex
  * different languages -> handling that
  * 			no hardcoded strings -> call from resources 
@@ -22,7 +21,6 @@ import javafx.beans.property.ObjectProperty;
  */
 public class CommandParser {
 
-	private Node treeNode;
     private List<String> commandList;
     private int indexofCommand;
     
@@ -31,14 +29,11 @@ public class CommandParser {
     
     //list of existing commands that are VALID
     //TODO EXTRACT THIS to another class
-    //IMPORTANT: command mapped to the number of arguments it takes
-    //this is in the command ->> command does error parsing
     private HashMap<String, Integer> validCommands = new HashMap<>();
 
     //regex command list -> unsure if needed?
     private List<Entry<String, Pattern>> regexCommmandList;
 
-    
     //in case of a loop statement:
     //currently not used
     private HashMap<String, Integer> loopCommandInfo = new HashMap<>();
@@ -49,33 +44,18 @@ public class CommandParser {
     private String loopVariable = ""; // needed or not?
     //similar thing for procedures and procedures/parameters
     
-    
     //in case of a conditional statement :
     private Boolean isConditional;
-    
-
+   
     //in case of another language
     private ResourceBundle getLang;
     private String language;
-    // to get commandsResourceBundle resources = ResourceBundle.getBundle(syntax);
+    //to get commandsResourceBundle resources = ResourceBundle.getBundle(syntax);
 
     //map of the variables created to the values made with
     private HashMap<String, Double> variable = new HashMap<>();
     
-	/**
-	 * 
-	 * not sure:
-	 * want command parser to always be listening  -> how to implement? another class prob
-	 * 
-	 * TO DO
-	 * Listener that checks to see if an instance variable in View changes and then running the command parsing if it has changed
-	 * steps:1. User inputs string and presses submit
-	 * 2. View's getText() method returns the String from the user Input and sets the View's CURRENTCOMMAND field to the String
-	 * 3. The listener in Controller sees that CURRENTCOMMAND has changed and grabs the String and passes it to Command Parser, starting the whole update process
-	 * 
-	 */
-
-    
+ 
 	/**
 	 * create arraylist of commands inputted
 	 * note: int values will be strings initially
@@ -92,6 +72,11 @@ public class CommandParser {
 	}
 	
 	
+	private String nthItemOfList(List<String> commandList, int n){
+		return commandList.get(n);
+
+	}
+	
     /**
      * @param string for the new node's command
      * @return new created Node
@@ -99,6 +84,7 @@ public class CommandParser {
     protected Node initNewNode(String commandString) {
         Node created = new Node(commandString);
         created.setCommand(commandString);
+        //uncomment this with the correct names for command info:
         //created.setCommandObjName(commandClassname.getcommandobject(commandString))
         return created;
     }
@@ -110,60 +96,52 @@ public class CommandParser {
 	 * Build tree from the list of commands
 	 * @return root of the new tree
 	 */
-	private Node buildTree(List<String> commandList){
-		Node rootNodeofTree = initNewNode(commandList.get(0));
-		//TODO implement check if null break
-		for(int i = 0; i < commandList.size(); i++){
-			String currCommand = commandList.get(i);
+	private Node buildTree(){
+		String currCommand = nthItemOfList(commandList,0);
+		commandList.remove(0);
+		Node rootNodeofTree = initNewNode(currCommand);
+		//keeping code in case recursion doesnt fully work
+		//for(int i = 0; i < commandList.size(); i++){
+			//String currCommand = commandList.get(i);
 			
 			//CASE1: if the word is make or set
 			if (currCommand.toLowerCase().equals("set") || currCommand.toLowerCase().equals("make")){
-				i++;
-				String newVariable = commandList.get(i);
-				i++;
-				Double valuetoAdd = Double.parseDouble(commandList.get(i));
+				String newVariable = commandList.get(1);
+				Double valuetoAdd = Double.parseDouble(commandList.get(2));
 				//check if its a variable and that it's NOT in map -> add it
 				if ((isVariable(newVariable))){
 					if(!isVariableinMap(newVariable)){
 						addVariableToHashmap(newVariable, valuetoAdd);
 					}
 				}
+				commandList.remove(1);
+				commandList.remove(2);
 			}
-			
+
+
 			//DEFAULT CASE:
-			Node currCommandNode = initNewNode(currCommand);	
-			
-			if(validCommands.containsKey(currCommand)){
+			Node currCommandNode = initNewNode(currCommand);
+			//Base case
+			if (currCommandNode.getNumberofChildren() == 0){
+				return currCommandNode; 
+			}
+			//TODO add in correct name for "numArgumentsmethodname" 
+			for (int i = 0; i < currCommandNode.getCommandObject().numArgumentsmethodname(); i++) { 
+				indexofCommand++;
+				currCommandNode.addChild(buildTree());
+			}		
+
+
+			//keeping in case recursive method implementation fails:
+			/*			if(validCommands.containsKey(currCommand)){
 				i++;
-				//move to the next command / value to add
-				String nextCommand = commandList.get(i);
-				Node nextCommandNode = initNewNode(nextCommand);
+				Node nextCommandNode = initNewNode(commandList.get(i));
 				currCommandNode.addChild(nextCommandNode);
 			}
-			i++;
-			
-			
-			
-/*			//implement to get nesting to work completely:
-			//create all children recursively
-	        for (int i = 0; i < newchildnode.getCommandObjectName().numArgumentsmethodname(); i++) { 
-	            myCommandIndex++;
-	            newchildnode.addChild(buildTree());
-	        }*/			
-		}
+			i++;*/
 		return rootNodeofTree;
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-
-	///NEXT 4 METHODS ARE TO DO WITH VARIABLE MAPPING AND RETURNING
 	/**
 	 * Checks if inputted string is a variable
 	 * checks for the ":"
@@ -188,8 +166,6 @@ public class CommandParser {
 	 * input: two arraylist indexes -> string name (including the colon) and INTEGER value to be added
 	 * NOTE: Must convert the value to an integer before its able to be added by this function
 	 * output: T/F depending on whether its a variable name or not
-	 * 
-	 * 
 	 * overall process:
 	 * check if its a variable 
 	 * 		if TRUE check if in map
@@ -201,17 +177,18 @@ public class CommandParser {
 	 */
 	private void addVariableToHashmap(String variableNameWithColon, Double valuetobeAdded){		
 		if (!(variablesinCurrentCommand.containsKey(variableNameWithColon))) {
-			//would we need to have a way to update the variable holding?
+			//would we need to have a way to update the variable holding? check on site
 			variablesinCurrentCommand.put(variableNameWithColon, valuetobeAdded);
 			}
 	}
 	
+	//TODO: implement fully
 	private Double returnStoredVal(String string){
 		return variablesinCurrentCommand.get(string);		
 	}
 	
-	
-	
+	//everything below this point is currently not implemented
+	//sad life
 	/**
 	 * Traverses command/node tree
 	 * returns most nested node
@@ -219,7 +196,7 @@ public class CommandParser {
 	 * @return Node
 	 */
 	//in execution
-	private Node getDeepestNode() {
+/*	private Node getDeepestNode() {
 		Node current = treeNode;
 		while (current != null) {
 
@@ -246,13 +223,7 @@ public class CommandParser {
 		return null;
 	}
 	
-	
-	//got to bottom -> execute the action and replace the parent node with the result of the children nodes
-	private void replaceParentonAction(){
-		
-	}
-	
-	
+*/	
 	
 	//loop through all children -> if all have "true" tag -> return true
 /*	private Boolean checkIfNodeExecuted(Node n){
@@ -265,31 +236,15 @@ public class CommandParser {
 		return true;
 	}
 	*/
-	
-	
-	
-	
+
 	//tree to list => list mapped to the commands
-	
+	//need or nah
 	private Node getCommandTreeToList(Node current){
 		return current;
-		
 	}
-
-
-
-	
-	
-	
-	
-	
-	
 	
 	//in case of a command loop
-	//how to go about it for tabbing? (can you tab and change loop), how to reset these variables?
-	//made this way to make it extendable -> no matter how the loop is
 	/**
-	 * 
 	 * How is it used: access these values in map if loop encountered -> use boolean to lead to this
 	 */
     protected void loopInformation(){
@@ -299,18 +254,16 @@ public class CommandParser {
     	loopCommandInfo.put("loopIncrement", loopIncrement);
     }
     
-    
 	/**
 	 * Set true is loop encountered -> need to update map in createCommandValues() -- "loopCommandInfo"
 	 */
     protected boolean isLoop(){
         return isLoop;
     }
-
-    
+ 
     
     //in case given a different language
-    
+    //has not been implemented
 	/**
 	 * Set Language -> how to detect in the first place??
 	 */
@@ -318,7 +271,6 @@ public class CommandParser {
     	language = lang;
         setTranslationMap();
     }
-
 	/**
 	 * Creating a translation mapping in case other language is inputted
 	 */
@@ -326,35 +278,9 @@ public class CommandParser {
     	getLang = ResourceBundle.getBundle("resources.languages/" + language);
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //regex !!!!! from in class
-    //not sure if any of the following is needed:
-    //CURRENTLY DOES NOT WORK
+ 
+    //regex from in class:
 /*    
-    
     *//**
      * Determine a string's type if it's a regex
      *
@@ -369,10 +295,7 @@ public class CommandParser {
             }
         }
         return ERROR;
-    }
-
-    
-    
+    }    
     
     *//**
      * Matches a String to a regular expression
@@ -384,9 +307,5 @@ public class CommandParser {
     private boolean isMatch(String command, Pattern regex) {
         return regex.matcher(command).matches();   //KEY LINE
     }*/
-
- 
-    
-    
 
 }
