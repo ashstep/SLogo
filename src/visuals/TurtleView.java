efaefa060456ecf7f384ba5c83f6499fd15e5cc2
Harry Liu <hkl6@duke.edu>
2017-03-03 15:43:59 -0500

    local tests of turtle with methods moved to TurtleView

---
+++ src/visuals/TurtleView.java
@@ -0,0 +1,129 @@
+package visuals;
+
+import java.io.InputStream;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import turtle.TurtleState;
+
+public class TurtleView {
+	
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	
+	
+	private StackPane stack;
+	private Canvas TurtleView;
+	private ImageView myTurtle;
+	private GraphicsContext myTurtleDrawer;
+	private double turtleXPos;
+	private double turtleYPos;
+	private double turtleAngle;
+	private String turtleImage = "turtleImage.png";
+	
+	protected double getTurtleX(){
+		return turtleXPos;
+	}
+	
+	protected double getTurtleY(){
+		return turtleYPos;
+	}
+	
+	protected double getTurtleAngle(){
+		return turtleAngle;
+	}
+	
+	/**
+	 * Initialize the left size of the BorderPane (the Canvas) which displays the turtle movements
+	 * @return TurtleView
+	 */
+	protected Canvas initializeGraphicContent() {
+		TurtleView = new Canvas (WIDTH*0.5, HEIGHT);
+		myTurtleDrawer = TurtleView.getGraphicsContext2D();
+
+		return TurtleView;
+	}
+	
+	protected ImageView initializeTurtle(){
+		turtleXPos = WIDTH/4;
+		turtleYPos = HEIGHT/2;
+		turtleAngle = 0;
+				
+		//myTurtle = new ImageView(myTurtleImage);
+		InputStream stream = getClass().getResourceAsStream(turtleImage);
+		myTurtle = new ImageView(new Image(stream));
+		System.out.println("stack is" + stack);
+		System.out.println("myTurtle is" + myTurtle);
+		
+		return myTurtle;
+	}
+	
+	/**
+	 * Filler method just to test if the line is displayed properly
+	 * @param x
+	 * @param y
+	 */
+	protected void drawTurtlePath(double xPosition, double yPosition, boolean pen){
+		myTurtleDrawer.moveTo(xPosition,yPosition);
+		if(pen) {
+			myTurtleDrawer.lineTo(xPosition, yPosition);		
+			myTurtleDrawer.stroke();
+		}
+	}
+	
+	public void updateTurtle(TurtleState newTurtle){
+		System.out.println("updateTurtle called");
+		
+		//stack.getChildren().remove(myTurtle);
+		
+		turtleXPos = newTurtle.getX();
+		turtleYPos = newTurtle.getY();
+		System.out.println("initial turtleXPos:" + turtleXPos);
+		System.out.println("initial turtleYPos:" + turtleYPos);
+
+		turtleAngle = newTurtle.getAngle();
+		
+		//setting our turtle info
+		myTurtle.setX(turtleXPos);
+		myTurtle.setY(turtleYPos);
+		
+		//Bug fixed here - needs more work on the subtraction though
+		myTurtle.setTranslateX(turtleXPos);
+		myTurtle.setTranslateY(turtleYPos);
+		
+		myTurtle.setRotate(turtleAngle); //how does setRotate work? absolute or relative angles?
+
+		//System.out.println(r.getBoundsInParent());
+		//stack.getChildren().remove(r);
+		//stack.getChildren().removeAll(r, TurtleView);
+
+		/*
+		stack.getChildren().add(updated);
+		System.out.println("got here");
+		System.out.println(r.getBoundsInParent());
+		System.out.println(updated.getBoundsInParent());
+		System.out.println("got here2");*/
+
+		//originally here:
+		/*r.setX(turtleXPos);
+		r.setY(turtleYPos);
+		stack.getChildren().add(r);*/
+		//.getChildren().addAll(r, TurtleView);
+
+		//System.out.println(r.getBoundsInParent());
+		//BP.setLeft(stack);
+		
+		drawTurtlePath(turtleXPos, turtleYPos, newTurtle.isPenDown());
+		
+		//stack.getChildren().add(myTurtle);
+		
+		System.out.println("final turtleXPos:" + turtleXPos);
+		System.out.println("final turtleYPos:" + turtleYPos);
+		
+		System.out.println("end of updateTurtle ");
+
+	}
+}
--- src/visuals/View.java
+++ src/visuals/View.java
@@ -15,12 +15,9 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import turtle.TurtleState;
 
@@ -40,6 +37,7 @@ public class View implements ExternalUserInterface {
 	public static final int HEIGHT = 600;
 	private final int SPACING = 10;
 	private InputView inputView;
+	private TurtleView turtleView = new TurtleView();
 	private Button help;
 	private ColorPicker backgroundColorChooser;
 	private ColorPicker strokeColorChooser;
@@ -47,12 +45,6 @@ public class View implements ExternalUserInterface {
 	private GraphicsContext gc;
 	private String helpUrl = 
 			"http://www.cs.duke.edu/courses/compsci308/spring17/assign/03_slogo/commands.php";
-	private GraphicsContext myTurtleDrawer;
-	private Image myTurtleImage;
-	private ImageView myTurtle;
-	private double turtleXPos;
-	private double turtleYPos;
-	private double turtleAngle;
 	private ResourceBundle myResourceBundle;
 	private Rectangle r;
 	private Rectangle updated;
@@ -81,20 +73,17 @@ public class View implements ExternalUserInterface {
 		System.out.println("reached here 4");
 		System.out.println("stack is " + stack);
 		
-		TurtleView = initializeGraphicContent();
+		TurtleView = turtleView.initializeGraphicContent();
 		inputView.setStroke(strokeColorChooser, gc);
-		stack.getChildren().addAll(TurtleView);
+		stack.getChildren().addAll(TurtleView, turtleView.initializeTurtle());
 		
 		System.out.println("stackchild is " + stack.getChildren());
 		
 		System.out.println("stack is INITIAL"+ stack);
 		
 		BP.setLeft(stack);
-
 		
 		//TurtleView = initializeGraphicContent();
-		
-		
 
 		theScene = new Scene(BP, WIDTH, HEIGHT);
 		theScene.getStylesheets().add(View.class.getResource("styles.css").toExternalForm());
@@ -121,39 +110,9 @@ public class View implements ExternalUserInterface {
 		RightMenu.getChildren().addAll(inputView.initializeTextArea(submit, myResourceBundle), backgroundLabel, backgroundColorChooser, lineColorLabel, strokeColorChooser, helpLabel, help);
 		return RightMenu;
 	}
-	
-	/**
-	 * Initialize the left size of the BorderPane (the Canvas) which displays the turtle movements
-	 * @return TurtleView
-	 */
-	private Canvas initializeGraphicContent() {
-		TurtleView = new Canvas (WIDTH*0.5, HEIGHT);
-		myTurtleDrawer = TurtleView.getGraphicsContext2D();
-		turtleXPos = WIDTH/4;
-		turtleYPos = HEIGHT/2;
-		turtleAngle = 0;
-				
-		//myTurtle = new ImageView(myTurtleImage);
-		myTurtle = new ImageView("unknown.png");
-		System.out.println("stack is" + stack);
-		System.out.println("myTurtle is" + myTurtle);
 
-		stack.getChildren().add(myTurtle);
-
-		return TurtleView;
-	}
-	
-	/**
-	 * Filler method just to test if the line is displayed properly
-	 * @param x
-	 * @param y
-	 */
-	private void drawTurtlePath(double xPosition, double yPosition, boolean pen){
-		myTurtleDrawer.moveTo(xPosition,yPosition);
-		if(pen) {
-			myTurtleDrawer.lineTo(xPosition, yPosition);		
-			myTurtleDrawer.stroke();
-		}
+	public void updateTurtle(TurtleState newTurtle){
+		turtleView.updateTurtle(newTurtle);
 	}
 	
 	/**
@@ -169,59 +128,6 @@ public class View implements ExternalUserInterface {
 		catch (URISyntaxException e){
 			System.out.println(myResourceBundle.getString("NotificationError"));
 		}
-	}
-
-	public void updateTurtle(TurtleState newTurtle){
-		System.out.println("updateTurtle called");
-		
-		//stack.getChildren().remove(myTurtle);
-		
-		turtleXPos = newTurtle.getX();
-		turtleYPos = newTurtle.getY();
-		System.out.println("initial turtleXPos:" + turtleXPos);
-		System.out.println("initial turtleYPos:" + turtleYPos);
-
-		turtleAngle = newTurtle.getAngle();
-		
-		//setting our turtle info
-		myTurtle.setX(turtleXPos);
-		myTurtle.setY(turtleYPos);
-		
-		//Bug fixed here - needs more work on the subtraction though
-		myTurtle.setTranslateX(turtleXPos);
-		myTurtle.setTranslateY(turtleYPos);
-		
-		myTurtle.setRotate(turtleAngle); //how does setRotate work? absolute or relative angles?
-
-		//System.out.println(r.getBoundsInParent());
-		//stack.getChildren().remove(r);
-		//stack.getChildren().removeAll(r, TurtleView);
-
-		/*
-		stack.getChildren().add(updated);
-		System.out.println("got here");
-		System.out.println(r.getBoundsInParent());
-		System.out.println(updated.getBoundsInParent());
-		System.out.println("got here2");*/
-
-		//originally here:
-		/*r.setX(turtleXPos);
-		r.setY(turtleYPos);
-		stack.getChildren().add(r);*/
-		//.getChildren().addAll(r, TurtleView);
-
-		//System.out.println(r.getBoundsInParent());
-		//BP.setLeft(stack);
-		
-		drawTurtlePath(turtleXPos, turtleYPos, newTurtle.isPenDown());
-		
-		//stack.getChildren().add(myTurtle);
-		
-		System.out.println("final turtleXPos:" + turtleXPos);
-		System.out.println("final turtleYPos:" + turtleYPos);
-		
-		System.out.println("end of updateTurtle ");
-
 	}
 	
 	/**
