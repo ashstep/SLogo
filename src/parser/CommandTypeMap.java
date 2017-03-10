93bc7f400cdbc7586fdf59e2429b97beccca7c38
Ashka <aas74@duke.edu>
2017-03-10 03:56:01 -0500

    parse full implementation

--- src/parser/CommandTypeMap.java
+++ src/parser/CommandTypeMap.java
@@ -12,24 +12,13 @@ import java.util.regex.Pattern;
 import command.Command;
 
 /**
- * Getting the correct command object
+ * Getting the correct command object no matter the sytax, such as languages and regex, through the use of reflections.
+ * Passed in any String and calls the correct Command object
  * @author Ashka Stephen
- *
- * this accounts for language, regex, etc
- * passed in any string and puts out the correct object
- * therefore needs to map all commands
- * TODO: fill in right paths for all resources?
- *
  */
 
 public class CommandTypeMap{
 	private List<Entry<String, Pattern>> mySymbols;
-
-	//forLANG	
-	//hardcoded currently
-	
-	//this works
-	//private static final String LANG = "resources.languages/English";
 	private static final String LANG = "resources.languages/";
 	private static final String SYNTAX = "resources.languages/Syntax";
 
@@ -37,81 +26,40 @@ public class CommandTypeMap{
 	 * Default constructor
 	 * @param String for input
 	 */
-	public CommandTypeMap(String lang, boolean set) {
+	public CommandTypeMap(String lang) {
 		mySymbols = new ArrayList<>();
-		if (set == true){
 			addResourceBundlez(LANG + lang);
 			addResourceBundlez(SYNTAX);
-		}
+		
 	}
 	/**
 	 * Using reflection properties to get command object
 	 * @param command String to m
 	 * @return Command object mapped to the command string input
 	 */  
-	public Command getCommandObj(String command) {
-		System.out.println("getCommandObj before: " + command);
-		
-		//hardcoded to make it work rn
+	public Command getCommandObj(String command) {		
+		//hardcoded remove
 		if (command.equals("Constant")){
 			System.out.println("its a Constatnt" );
 			command = "turtlecommands." + command;
-			
+
 		}
-		System.out.println("getCommandObj after: " + command);
-
-		//added to chekc for contsantt
-	    ResourceBundle constant = ResourceBundle.getBundle("classinformation/ClassLocations");
-        //String commandType = getCommandType(command);
-		
-		
-        //String commandType = getCommandString(command);
-		System.out.println("aftaaa");
-
-		System.out.println("command:" + command);
-
-        //correct address? is this even needed?
+		ResourceBundle constant = ResourceBundle.getBundle("classinformation/ClassLocations");
 		try {
-			System.out.println("CommandTypeMap: command being called for class is:  " + command);
-			System.out.println("before class called");
-		
-			//previous
-			//Class<?> commandObjectClazz = Class.forName(command); 
-		
-			
 			Class<?> commandObjectClazz = (command == "Constant") ? Class.forName(constant.getString(command)) : Class.forName(command);
-
-
-			//if its a constant
-			//if(command == "Constant"){Class<?> commandObjectClazz = Class.forName(constant.getString(command)); }
-			
-			
-			System.out.println("CommandTypeMap: class found");
 			try {
 				Constructor<?> commandObjConstructor = commandObjectClazz.getDeclaredConstructor(); //getting the constructor
-				System.out.println("CommandTypeMap: constructor found");
-
 				Object commandObject = commandObjConstructor.newInstance(); //create instance
-				System.out.println("CommandTypeMap: obj found");
-
 				return (Command) commandObject;
 			} catch(Exception e) {
-				System.out.println("CommandTypeMap: constructor or obj  NOT found");
-
 				e.printStackTrace();
 			}
 		} catch(Exception e) {
-			System.out.println("Class NOT found");
 			e.printStackTrace();
 		}
 		return null;
 	}
 	
-	
-	/**
-	 * LANGUAGES =========================
-	 */
-
 	/**
 	 * Adds the given resource file to this language's recognized types
 	 *
@@ -120,15 +68,12 @@ public class CommandTypeMap{
 	 */
 	public void addResourceBundlez(String filecalled) {
 		ResourceBundle resources = ResourceBundle.getBundle(filecalled);
-		System.out.println("resource bundle in commandtype map called: " + filecalled);
 		Enumeration<String> iter = resources.getKeys();
 		while (iter.hasMoreElements()) {
 			String key = iter.nextElement();
 			String regex = resources.getString(key);
 			mySymbols.add(new SimpleEntry<>(key, Pattern.compile(regex, Pattern.CASE_INSENSITIVE)));
 		}
-		System.out.println("resource bundle finished");
-
 	}
 	
 	
@@ -137,28 +82,17 @@ public class CommandTypeMap{
 	 * @return the language's type associated with the given text if one exists
 	 */
 	public String getCommandString(String command) {
-		System.out.println("command string inputted in getCommandString : " + command);
-
-		final String ERROR = "CommandTypeMap, getCommandString: NO STRING MATCH FOUND";
+		final String ERROR = "NO STRING MATCH FOUND";
 		for (Entry<String, Pattern> e : mySymbols) {
-
 			if (match(command, e.getValue())) {
-				System.out.println("looping through to get command string, match successful!!");
-				System.out.println(e.getKey());
-
 				return e.getKey();
 			}
 		}
 		return ERROR;
 	}
-
-
-
+	
 	/**
-	 * REGEX  =========================
-	 */    
-
-	/**
+	 * Regular Expressions
 	 * @return true if the given text matches the given regular expression pattern
 	 */
 	private boolean match(String command, Pattern regex) {
--- src/parser/ParserHelper.java
+++ src/parser/ParserHelper.java
@@ -2,121 +2,117 @@ package parser;
 
 import java.util.ArrayList;
 
-//recursievly call command parser and create trees and execute ocmmands as you go 
-
+/**
+ *  Recursievly call command parser and create trees and execute Commands as you go
+ * 
+ * @author Ashka Stephen
+ */
 public class ParserHelper {
 	CommandTypeMap theCommandMapObject;
 	CommandParser newParse;
-    ArrayList<Node> finallist = new ArrayList<Node>();
-	
-    /**
-     * Initializes a new command parsing builder
-     */
-    private CommandParser initTreeCreation() {
-    	CommandParser newParse = new CommandParser();
-    	return newParse;
-    	}
+	ArrayList<Node> finalparserlist = new ArrayList<Node>();
 
+	/**
+	 * Initializes a new command parsing builder
+	 */
+	private CommandParser initTreeCreation() {
+		CommandParser newParse = new CommandParser();
+		return newParse;
+	}
 
+	/**
+	 * Builds a tree to be parse with commands
+	 * @param commands String array representing commands
+	 * @return Double for result of each execution
+	 */
 
-    /**
-     * Builds a tree to be parse with commands
-     * @param commands String array representing commands
-     * @return Double for result of each execution
-     */
+	private double buildTree(String[] commandListInput) {
+		System.out.println("started PARSERHELPER");
+		CommandParser newTreeParse = initTreeCreation();
+		newTreeParse.initTreeRecurse(commandListInput);
+		ArrayList<Node> a = newTreeParse.getCurrentList();
 
-    private double buildTree(String[] commandListInput) {
-    	//TO DO: check for errors?
-    	System.out.println("started PARSERHELPER");
-    	
-    	
-    	
-    	CommandParser newTreeParse = initTreeCreation();
-    	//head node of the tree for current command
-		System.out.println("right BEFORE tree is iniitalized");
-
-    	Node headNode = newTreeParse.initTreeRecurse(commandListInput);
-    	ArrayList<Node> a = newTreeParse.getFinalArrayList();
-    	
-    	//taking stuff from other and adding it here
-    	for(int i =0; i< a.size(); i++){
-    		finallist.add(a.get(i));
-    	}
-		System.out.println("PARSERHELPER FINAL ARRAYLIST size " + finallist.size());
-
-    	
-    	
-		System.out.println("right aftter tree is iniitalized");
+		//extract: get current executables
+		for(int i =0; i< a.size(); i++){
+			finalparserlist.add(a.get(i));
+		}
+		System.out.println("PARSERHELPER FINAL ARRAYLIST size " + finalparserlist.size());
 		
-		System.out.println("geCommandListIndex is : " + newTreeParse.geCommandListIndex());
-		System.out.println("commandListInput length (sub 1 later) is:" + commandListInput.length );
+		
+		int curr = newTreeParse.geCommandListIndex();
+		System.out.println("currently at : " + curr);
 
+		int test = commandListInput.length-1;
+		System.out.println("is less than " + test );
 
-		while(newTreeParse.geCommandListIndex() < commandListInput.length - 1){
+		if(curr < test){
+			System.out.println("does it even get");
+
 			//we need to do this for all commands
 			newTreeParse.incrCommandListIndex();
 			//go to next one and redo
 			int newstart = newTreeParse.geCommandListIndex();
-			System.out.println("newTreeParse index start   " + newstart);
 			
+			
+			
+			System.out.println("newTreeParse index start   " + newstart);
 			int j = newstart;
-
 			System.out.println("loop starts at    " + j);
 			System.out.println("commandListInput.length    " + commandListInput.length);
-
 			int acount= 0;
-	        String[] abcd = new String[commandListInput.length-j];
+			String[] abcd = new String[commandListInput.length-j];
 			System.out.println("new string is size " + abcd.length);
-
 			for(int i = j; i < commandListInput.length; i++){
-				System.out.println("entering in the loop " );
-				System.out.println("i is " + i);
-				System.out.println("commandListInput[i] is  " + commandListInput[i]);
+				System.out.println(" in loop commandListInput[i] is  " + commandListInput[i]);
 				abcd[acount] = commandListInput[i];
 				acount++;
 			}
-			
+
+			//printing the vals of ABCD
 			for (int i = 0; i< abcd.length; i++){
 				System.out.println("abcd[i] is  " + abcd[i]);
 			}
+
 			if(abcd.length == 1){
+				System.out.println("BREAK");
+				//break;
 				return 0.0;
 			}
-			
 			System.out.println("this should work recurse");
-					
 			buildTree(abcd);
-
 		}
 		//System.out.println(theCommandMapObject.getCommandObj(headNode.getCommand()).findReturnVal(headNode));
 		return 0.0;//theCommandMapObject.getCommandObj(headNode.getCommand()).findReturnVal(headNode);
-    }
+	}
 
-    public ArrayList<Node> getFinalArrayList () {
-        System.out.println("PARSERHELPER FINAL ARRAYLIST getFinalArrayList of size " + finallist.size());
-        return finallist;
-    }
-    public void resetArrayList() {
-    	finallist.clear();
-        System.out.println("PARSERHELPER ARRAYCLEAR ");
-    	}
+	public ArrayList<Node> getFinalArrayList () {
+		System.out.println("PARSERHELPER FINAL ARRAYLIST getFinalArrayList of size " + finalparserlist.size());
+		return finalparserlist;
+	}
 
-    
-    /**
-     * Executes the TOTAL actions for all commands
-     */
-    public double valuesForSequentialCommandExecution(String[] commands) {
-    	System.out.println("successfully in valuesForSequentialCommandExecution ");
-        return buildTree(commands);
-    }
-    
-    
-	//for testing
+
+	/**
+	 * Initializes the treeBuilder for the value propagation
+	 */
+	public double valuesForSequentialCommandExecution(String[] commands) {
+		System.out.println("successfully in valuesForSequentialCommandExecution ");
+		return buildTree(commands);
+	}
+
+
+	/**
+	 * Executes the TOTAL actions for all commands
+	 */
 	public double parserhelperparsecommand(String commandLineInput) {
-    	System.out.println("parserhelperparsecommand ");
 		String[] inputtedCommandsList = commandLineInput.split(" ");
 		return valuesForSequentialCommandExecution(inputtedCommandsList);
 	}
-    
-    
+
+	/**
+     * Resets the ArrayList once the initial String input has completelty finished 
+     */
+    public void resetArrayList() {
+    	finalparserlist.clear();
+    	System.out.println("PARSERHELPER ARRAYCLEAR ");
+    }
 }
